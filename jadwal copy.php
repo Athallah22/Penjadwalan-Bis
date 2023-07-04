af23fd4a51cab74170d2ba3337e87732d5097912
Hamdan Al Atsari <114043304+HamdanAlAtsari2200018002@users.noreply.github.com>
2023-07-04 07:17:19 +0700

    Add files via upload

---
+++ jadwal copy.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Pweb</title>
+
+    <link rel="stylesheet" href="stylejadwal2.css">
+    <link rel="stylesheet" href="stylejadwal.css">
+</head>
+
+<body class="main-content">
+    <header class="header">
+    <a href="#" class="logo">Dahlan Bus</a>
+        <nav class="navbar">
+            <a href="index.html" class="active">Home</a>
+            <a href="jadwal.php">Jadwal</a>
+            <button class="btnLogin-popup">
+                <a href="Register.php">Login</a></button>
+        </nav>
+    </header>
+
+    <section class="home" id="home">
+        <div class="home-kontent">
+        <div class="data-table-wrapper">
+            <table class="data-table">
+                <caption class="title">JADWAL KEBERANGKATAN BUS</caption>
+                <thead>
+                    <tr>
+                        <th>NO</th>
+                        <th>BUS</th>
+                        <th>TUJUAN</th>
+                        <th>KELAS</th>
+                        <th>HARGA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer-kontent">
+            <p>&copy; 2023 Dahlan Bus. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
